81088ba33b86b84b455ce2206e53ffcae1f914b2
willcoderbr <williamribbeiro94@gmail.com>
2023-07-04 17:39:20 -0300

    Update Curso

--- EXERCICIOS-JS/fundamentos/numerosAlgunsCuidados.js
+++ EXERCICIOS-JS/fundamentos/numerosAlgunsCuidados.js
@@ -1,5 +1,6 @@
 console.log(7 / 0) // dividir por zero = infinito
 console.log("10" / 2)
+console.log('3' + 2) // na soma, o string prevalece 
 console.log("Show" * 2) // NaN - Not a Number
 console.log(0.1 + 0.7)
 // console.log(10.toString())
